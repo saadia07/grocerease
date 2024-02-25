5c75eea82d0fbd6753f597e1f14b1fb50c4bb379
saadia07 <sadia.shahzad703@gmail.com>
2024-02-25 22:19:56 +0300

    js login

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
-if(username == 'Admin' && password=='admin123'){
-    document.getElementById(password && username)
+function login()
+{
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    // console.log(username,password);
+    if(username=="admin" && password=="admin123")
+    {
+      alert(
+        "you are successfully logged"
+      )
+    } else {
+        alert(
+            "wrong username and password"
+        )
+    }
 }
